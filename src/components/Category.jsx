28335b6b3d210da40a0c73679dfb633271a27fdb
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-10-01 16:23:44 +0330

    new fix

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -13,7 +13,7 @@ const Category = ({ filterHandler, setFilterCategory }) => {
     <>
       <div
         id="filter_list"
-        className="relative md:flex-wrap flex-wrap bottom-[9.4rem] top-[-7rem] xl:top-[-7rem] 2xl:top-[-7rem] mx-0 my-[4rem] flex justify-center md:justify-start items-center gap-16 pt-28 md:pt-12"
+        className="relative md:flex-wrap flex-wrap bottom-[9.4rem] top-[-7rem] xl:top-[-7rem] 2xl:top-[-7rem] mx-0 my-[4rem] flex justify-center md:justify-start items-center gap-16 pt-40 md:pt-12"
       >
         <CategoryItems
           region={region}
