5f88b284c0552fe9078ef56e429f51e1548a5f2e
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-10-02 09:15:27 +0330

    update components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="flex justify-center item-center p-9 border-4 border-[#756AB6] rounded-3xl bg-[#756AB6] text-black font-black">
+        <p>Develop with ❤️‍🔥 by Reza</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Category from "../components/Category";
+import Header from "../components/Header";
 import BannerSlider from "../components/BannerSlider";
-import { NavLink } from "react-router-dom";
+import Category from "../components/Category";
+import Footer from "../components/Footer";
 
 // ICON
-import { FaGithub } from "react-icons/fa";
-import { BiSearchAlt } from "react-icons/bi";
 
 const Layout = ({
   children,
@@ -17,57 +16,14 @@ const Layout = ({
 }) => {
   return (
     <>
-      <header className="flex lg:justify-between  sm:justify-center   justify-center p-9 border-4 border-[#756AB6] rounded-3xl bg-[#756AB6] text-black">
-        <div className="hidden justify-center items-center gap-8 sm:hidden lg:flex">
-          <h3 className="text-3xl font-black select-none">
-            RezaShirali - ReactJS
-          </h3>
-          <a
-            href="https://github.com/Reza-Shirali"
-            target="_blank"
-            className="flex justify-center item-center"
-          >
-            <FaGithub className="text-6xl" />
-          </a>
-        </div>
-        <div className="flex gap-16 content-center items-center">
-          <ul className="hidden m-0 font-extrabold justify-center items-center sm:gap-4 md:gap-4 lg:gap-16 sm:hidden md:flex">
-            <NavLink to="/">
-              <li className="cursor-pointer max-w-[9rem]">Home</li>
-            </NavLink>
-            <NavLink to="contact-us">
-              <li className="cursor-pointer max-w-[9rem]">Contact Us</li>
-            </NavLink>
-            <NavLink to="about-us">
-              <li className="cursor-pointer max-w-[9rem]">About Us</li>
-            </NavLink>
-          </ul>
-          <div className="flex justify-center items-center gap-4  p-4 border-2 rounded-3xl border-[#E0AED0]">
-            <input
-              type="text"
-              placeholder="Search by name, description and price..."
-              className="min-w-[28rem] bg-transparent h-[100%] outline-none placeholder-gray-800"
-              value={searchInput}
-              onChange={(e) => setSearchInput(e.target.value)}
-            />
-            <div
-              className="flex justify-center item-center"
-              onClick={searchHandler}
-            >
-              <BiSearchAlt className="text-5xl cursor-pointer" />
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header searchInput={searchInput} setSearchInput={setSearchInput} searchHandler={searchHandler}/>
       <BannerSlider />
       <Category
         setFilterCategory={setFilterCategory}
         filterHandler={filterHandler}
       />
       {children}
-      <footer className="flex justify-center item-center p-9 border-4 border-[#756AB6] rounded-3xl bg-[#756AB6] text-black font-black">
-        <p>Develop with ❤️‍🔥 by Reza</p>
-      </footer>
+      <Footer/>
     </>
   );
 };
